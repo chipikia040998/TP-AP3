2b537593739dc3171eac9d0c37ac32a9dd94c080
camille <57714750+chipikia040998@users.noreply.github.com>
2021-12-06 14:22:48 +0000

    entete dans .c

--- src/ArbreSyntaxiques.c
+++ src/ArbreSyntaxiques.c
@@ -4,3 +4,80 @@
 #include <stdlib.h>
 //inclusion de fichier
 #include "../include/ArbresSyntaxiques.h"
+
+/*************************************************/
+
+TArbreSynt ConsVariable()
+{
+	//TO DO
+}
+
+/*************************************************/
+
+TArbreSynt ConsConstante(double v)
+{
+	//TO DO
+}
+
+/*************************************************/
+
+TArbreSynt ConsBinaire(char Op, TArbreSynt FG, TArbreSynt FD)
+{
+	//TO DO
+}
+
+/*************************************************/
+
+TArbreSynt ConsFonction(char Fct, TArbreSynt F)
+{
+	//TO DO
+}
+
+/*************************************************/
+
+TNature Nature(TArbreSynt A)
+{
+	//TO DO
+}
+
+/*************************************************/
+
+double ValeurConstante(TArbreSynt A)
+{
+	//TO DO
+}
+
+/*************************************************/
+
+char Operateur(TArbreSynt A)
+{
+	//TO DO
+}
+
+/*************************************************/
+
+TArbreSynt FG(TArbreSynt A)
+{
+	//TO DO
+}
+
+/*************************************************/
+
+TArbreSynt FD(TArbreSynt A)
+{
+	//TO DO
+}
+
+/*************************************************/
+
+char NomFonction(TArbreSynt A)
+{
+	//TO DO
+}
+
+/*************************************************/
+
+TArbreSynt Argument(TArbreSynt A)
+{
+	//TO DO
+}
